c901d3b44fa0366bec8259f39dca65c26e48e911
Gabrielgermanoo <gabriel.lucasgermano@gmail.com>
2022-06-30 16:34:16 -0300

    Import made

--- Project/comp/models.py
+++ Project/comp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Login(models.Model):
     id = models.AutoField(primary_key=True)
-    login = models.CharField(max_length=50, unique= True)
+    login = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
     class Meta:
--- Project/comp/resources.py
+++ Project/comp/resources.py
@@ -1,11 +1,13 @@
 from import_export import resources
 from comp.models import Users
 
-class MemberResource(resources.ModelResource):
+class UserResource(resources.ModelResource):
     class Meta:
         model = Users
         skip_unchanged = True
         report_skipped = True
-        import_id_fields = ('login',)
-        exclude = ('id')
+        import_id_fields = ('id', 'login', 'password')
+
+    def skip_row(self, instance, original):
+        return super().skip_row(instance, original)
     
